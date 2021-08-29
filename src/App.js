c1b56bd81455d1bcaaa8d267decfe58cca081bab
EminGm <qasimovemin@gmail.com>
2021-08-29 22:31:21 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className = "wrapper">
+      <section className = "header">
+        <div className = "header__logo">
+          <a href="#"><img src="" alt=""></a>
+        </div>
+        <div className="header__profile"></div>
+      </section>
+      <section className = "sidebar">Start editing to see some magic happen :)</section>
+      <section className = "content">Start editing to see some magic happen :)</section>
+    </div>
+  );
+}
