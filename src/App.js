7be4dd932f585c5248b0d9f3d2a1a04d7f79c86d
EminGm <qasimovemin@gmail.com>
2021-08-30 12:27:52 +0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ export default function App() {
     <div className = "wrapper">
       <section className = "header">
         <div className = "header__logo">
-          <a href="#"><img src="" alt=""></a>
+          <a href="#"><img src="https://thumbs.dreamstime.com/z/greek-goddess-antique-decorative-frame-portrait-circle-frame-vector-logo-design-template-antique-coin-greek-goddess-160741805.jpg" alt=""></img></a>
         </div>
         <div className="header__profile"></div>
       </section>
-      <section className = "sidebar">Start editing to see some magic happen :)</section>
-      <section className = "content">Start editing to see some magic happen :)</section>
+      <section className = "sidebar">
+        <ul>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Messages</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Music</a></li>
+        <li><a href="#">Settings</a></li>
+      </ul>
+      </section>
+      <section className = "content">
+        <div className="content__img">
+          <img src="https://www.uu.se/digitalAssets/805/c_805646-l_1-k_image.jpg" alt="content_img"></img></div>Start editing to see some magic happen :)</section>
     </div>
   );
 }
