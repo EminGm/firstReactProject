999df07b2e5268539f9c1b58161595dc098a34a7
EminGm <qasimovemin@gmail.com>
2021-09-08 14:04:41 +0400

    feat:Add Navlink to messages

--- my-app/src/components/content/messages/messages.jsx
+++ my-app/src/components/content/messages/messages.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import style from "./messages.module.css";
-
+import { NavLink } from "react-router-dom";
 
 const Messages = () => {
   return (
     <section className={style.messages}>
       <div className={style.dialogs}>
-        <div className={style.dialog}>Name1</div>
-        <div className={style.dialog}>Name2</div>
-        <div className={style.dialog}>Name3</div>
-        <div className={style.dialog}>Name4</div>
-        <div className={style.dialog}>Name5</div></div>
+        <div className={style.dialog}><NavLink to="/dialogs/Name1">Name1</NavLink></div>
+        <div className={style.dialog}><NavLink to="/dialogs/Name2">Name2</NavLink></div>
+        <div className={style.dialog}><NavLink to="/dialogs/Name3">Name3</NavLink></div>
+        <div className={style.dialog}><NavLink to="/dialogs/Name4">Name4</NavLink></div>
+        <div className={style.dialog}><NavLink to="/dialogs/Name5">Name5</NavLink></div>
+      </div>
       <div className={style.chats}>
         <div className={style.chat}>Hi there</div>
         <div className={style.chat}> Hello</div>
