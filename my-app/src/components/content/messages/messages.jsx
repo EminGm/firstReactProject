27f9ed2ace0eb206e27edf43f4e56410aa3d8d69
EminGm <qasimovemin@gmail.com>
2021-09-08 22:40:29 +0400

    feat:Add Bll to messages

--- my-app/src/components/content/messages/messages.jsx
+++ my-app/src/components/content/messages/messages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./messages.module.css";
 import { NavLink } from "react-router-dom";
 
+
 const Dialogs = (props) => {
   return (<div className={style.dialog}><NavLink to={props.id}>{props.name}</NavLink></div>)
 }
@@ -12,21 +13,35 @@ const Chat = (props) => {
   )
 }
 
+let dialogsData = [
+  { id: "/dialogs/Name1", name: "Name1" },
+  { id: "/dialogs/Name2", name: "Name2" },
+  { id: "/dialogs/Name3", name: "Name3" },
+  { id: "/dialogs/Name4", name: "Name4" },
+  { id: "/dialogs/Name5", name: "Name5" }];
+
+let chatsData = [
+  { id: "1", message: "Hi there" },
+  { id: "2", message: "How r U" },
+  { id: "3", message: "Go go go" },
+  { id: "4", message: "Nice day" },
+];
+
 const Messages = () => {
   return (
     <section className={style.messages}>
       <div className={style.dialogs}>
-        <Dialogs id="/dialogs/Name1" name="Name1" />
-        <Dialogs id="/dialogs/Name2" name="Name2" />
-        <Dialogs id="/dialogs/Name3" name="Name3" />
-        <Dialogs id="/dialogs/Name4" name="Name4" />
-        <Dialogs id="/dialogs/Name5" name="Name5" />
+        <Dialogs id={dialogsData[0].id} name={dialogsData[0].name} />
+        <Dialogs id={dialogsData[1].id} name={dialogsData[1].name} />
+        <Dialogs id={dialogsData[2].id} name={dialogsData[2].name} />
+        <Dialogs id={dialogsData[3].id} name={dialogsData[3].name} />
+        <Dialogs id={dialogsData[4].id} name={dialogsData[4].name} />
       </div>
       <div className={style.chats}>
-        <Chat message="Hi there" />
-        <Chat message="How r U" />
-        <Chat message="Go go go" />
-        <Chat message="Nice day" />
+        <Chat id={chatsData[0].id} message={chatsData[0].message} />
+        <Chat id={chatsData[1].id} message={chatsData[1].message} />
+        <Chat id={chatsData[2].id} message={chatsData[2].message} />
+        <Chat id={chatsData[3].id} message={chatsData[3].message} />
       </div>
     </section>);
 }
