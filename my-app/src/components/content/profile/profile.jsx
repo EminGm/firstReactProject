0d68c1fd31c55c5e5cf991b866a4b58466400a72
EminGm <qasimovemin@gmail.com>
2021-09-08 17:58:49 +0400

    refactor:Separate profile to components

--- my-app/src/components/content/profile/profile.jsx
+++ my-app/src/components/content/profile/profile.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import style from "./profile.module.css";
 import Posts from "./posts/posts.jsx"
+import Profileinfo from "./profileinfo/profileinfo";
 
 const Profile = () => {
   return (
     <section className={style.content}>
-      <div className="content__img">
-        <img src="https://www.uu.se/digitalAssets/805/c_805646-l_1-k_image.jpg" alt="content_img"></img>
-      </div>
+      <Profileinfo />
       <Posts message="Test" />
       <Posts message="Test2" />
       <Posts message="Test3" />
---
+++ my-app/src/components/content/profile/profileinfo/profileinfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./profileinfo.module.css";
+
+
+const Profileinfo = () => {
+  return (
+    <section className={style.content}>
+      <div className="content__img">
+        <img src="https://www.uu.se/digitalAssets/805/c_805646-l_1-k_image.jpg" alt="content_img"></img>
+      </div>
+    </section>);
+}
+
+export default Profileinfo;
