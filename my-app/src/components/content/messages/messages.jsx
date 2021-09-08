a802cb51adb7f1790fed6c640db10aba5af283af
EminGm <qasimovemin@gmail.com>
2021-09-08 17:38:23 +0400

    refactor:Separate messages to components

--- my-app/src/components/content/messages/messages.jsx
+++ my-app/src/components/content/messages/messages.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import style from "./messages.module.css";
 import { NavLink } from "react-router-dom";
 
+const Dialogs = (props) => {
+  return (<div className={style.dialog}><NavLink to={props.id}>{props.name}</NavLink></div>)
+}
+
+const Chat = (props) => {
+  return (
+    <div className={style.chat}>{props.message}</div>
+  )
+}
+
 const Messages = () => {
   return (
     <section className={style.messages}>
       <div className={style.dialogs}>
-        <div className={style.dialog}><NavLink to="/dialogs/Name1">Name1</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/Name2">Name2</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/Name3">Name3</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/Name4">Name4</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/Name5">Name5</NavLink></div>
+        <Dialogs id="/dialogs/Name1" name="Name1" />
+        <Dialogs id="/dialogs/Name2" name="Name2" />
+        <Dialogs id="/dialogs/Name3" name="Name3" />
+        <Dialogs id="/dialogs/Name4" name="Name4" />
+        <Dialogs id="/dialogs/Name5" name="Name5" />
       </div>
       <div className={style.chats}>
-        <div className={style.chat}>Hi there</div>
-        <div className={style.chat}> Hello</div>
-        <div className={style.chat}> How r U</div>
+        <Chat message="Hi there" />
+        <Chat message="How r U" />
+        <Chat message="Go go go" />
+        <Chat message="Nice day" />
       </div>
     </section>);
 }
