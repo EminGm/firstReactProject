725e02e32ddeb1fbde966032db2ac567bddb6226
EminGm <qasimovemin@gmail.com>
2021-09-22 13:02:37 +0400

    feat:Add state file

--- my-app/src/components/content/profile/profile.jsx
+++ my-app/src/components/content/profile/profile.jsx
@@ -3,13 +3,6 @@ import style from "./profile.module.css";
 import Posts from "./posts/posts.jsx"
 import Profileinfo from "./profileinfo/profileinfo";
 
-/*let postsData = [
-  { id: "1", message: "Hi there", likesCount: 1 },
-  { id: "2", message: "How r U", likesCount: 4 },
-  { id: "3", message: "Go go go", likesCount: 41 }
-];
-*/
-
 
 const Profile = (props) => {
 
---
+++ my-app/src/components/state/state.js
@@ -0,0 +1,30 @@
+
+
+
+let state = {
+    messagesPage: {
+        dialogsdata: [
+            { id: "/dialogs/Name1", name: "Name1" },
+            { id: "/dialogs/Name2", name: "Name2" },
+            { id: "/dialogs/Name3", name: "Name3" },
+            { id: "/dialogs/Name4", name: "Name4" },
+            { id: "/dialogs/Name5", name: "Name5" }],
+
+        chatsdata: [
+            { id: "1", message: "Hi there" },
+            { id: "2", message: "How r U" },
+            { id: "3", message: "Go go go" },
+            { id: "4", message: "Nice day" },
+        ]
+    }
+    ,
+    profilePage: {
+        postsData: [
+            { id: "1", message: "Hi there", likesCount: 1 },
+            { id: "2", message: "How r U", likesCount: 4 },
+            { id: "3", message: "Go go go", likesCount: 41 }
+        ]
+    }
+
+};
+
